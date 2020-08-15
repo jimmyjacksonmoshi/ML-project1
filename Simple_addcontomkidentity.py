1ce61dc30b4e30d6de42f24567e2ce311c7a76a2
jimmyjacksonmoshi <jimmymoshi1@gmail.com>
2020-08-15 16:18:43 +0300

    ResNet and ResNet50 models with their architectural diagrams

---
+++ Simple_addcontomkidentity.py
@@ -0,0 +1,176 @@
+# code for loading the format for the notebook
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import keras.layers as layers
+from keras.models import Model
+from keras.preprocessing import image
+from keras.initializers import glorot_uniform
+# 1. magic so that the notebook will reload external python modules
+# 2. magic for inline plot
+# 3. magic to enable retina (high resolution) plots
+# https://gist.github.com/minrk/3301035
+# 4. magic to print version
+
+def identity_block(input_tensor, kernel_size, filters, stage, block):
+    """
+    An identity block.
+
+    Parameters
+    ----------
+    input_tensor:
+
+    kernel_size: int
+        The kernel size of middle conv layer at main path.
+
+    filters: list[int]
+        The filters of 3 conv layer at main path.
+
+    stage: int
+        Current stage label, used for generating layer names.
+
+    block: : str
+        'a','b'..., current block label, used for generating layer names.
+
+    Returns
+    -------
+    Output tensor for the block.
+    """
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # for batch normalization layer, we assume
+    # the input data is in channel last format
+    bn_axis = 3
+
+    filters1, filters2, filters3 = filters
+
+    # main path, note that setting the kernel_initializer seed here is only used
+    # for reproducibility, we techniqually don't need it
+    x = layers.Conv2D(filters1, kernel_size=(1, 1), strides=(1, 1),
+                      kernel_initializer=glorot_uniform(seed=0),
+                      padding='valid', name=conv_name_base + '2a')(input_tensor)
+    x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv2D(filters2, kernel_size, strides=(1, 1),
+                      kernel_initializer=glorot_uniform(seed=0),
+                      padding='same', name=conv_name_base + '2b')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv2D(filters3, kernel_size=(1, 1), strides=(1, 1),
+                      kernel_initializer=glorot_uniform(seed=0),
+                      padding='valid', name=conv_name_base + '2c')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    # this line is the core component of resnet, the skip connection, i.e.
+    # having a shortcut to the main path before the activation, when addition
+    # is performed on convolutional layers, the element-wise addition is performed
+    # on their feature maps, i.e. channel by channel
+    x = layers.add([x, input_tensor])
+    x = layers.Activation('relu')(x)
+    return x
+
+
+
+def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
+    """
+    A block that has a conv layer at shortcut.
+
+    Parameters
+    ----------
+    input_tensor:
+
+    kernel_size: int
+        The kernel size of middle conv layer at main path.
+
+    filters: list[int]
+        The filters of 3 conv layer at main path.
+
+    stage: int
+        Current stage label, used for generating layer names.
+
+    block: : str
+        'a','b'..., current block label, used for generating layer names.
+
+    strides : tuple, default (2, 2)
+        Strides for the first conv layer in the block.
+
+    Returns
+    -------
+    Output tensor for the block.
+    """
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # for batch normalization layer, we assume
+    # the input data is in channel last format,
+    # which is the case if we are using the default
+    # keras' backend tensorflow
+    bn_axis = 3
+
+    filters1, filters2, filters3 = filters
+
+    # main path, note that setting the kernel_initializer set here is only used
+    # for reproducibility, we techniqually don't need it
+    x = layers.Conv2D(filters1, kernel_size=(1, 1), strides=strides,
+                      kernel_initializer=glorot_uniform(seed=0),
+                      padding='valid', name=conv_name_base + '2a')(input_tensor)
+    x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv2D(filters2, kernel_size, strides=(1, 1),
+                      kernel_initializer=glorot_uniform(seed=0),
+                      padding='same', name=conv_name_base + '2b')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv2D(filters3, kernel_size=(1, 1), strides=(1, 1),
+                      kernel_initializer=glorot_uniform(seed=0),
+                      padding='valid', name=conv_name_base + '2c')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    # we resize the input so its dimension will match the output dimension
+    # of the main path
+    shortcut = layers.Conv2D(filters3, kernel_size=(1, 1), strides=strides,
+                             kernel_initializer=glorot_uniform(seed=0),
+                             padding='valid', name=conv_name_base + '1')(input_tensor)
+    shortcut = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    # this line is the core component of resnet, the skip connection, i.e.
+    # having a shortcut to the main path before the activation
+    x = layers.add([x, shortcut])
+    x = layers.Activation('relu')(x)
+    return x
+
+
+# generate some fake data to work with
+np.random.seed(0)
+X = np.random.randn(3, 4, 4, 6)
+print('original data shape:', X.shape)
+
+stage = 1
+block = 'a'
+inputs = layers.Input(shape=X.shape[1:])
+
+outputs = identity_block(inputs, kernel_size=2, filters=[2, 4, 6], stage=stage, block=block)
+
+model = Model(inputs=inputs, outputs=outputs)
+prediction = model.predict(X)
+print('identity block output shape:', prediction.shape)
+prediction[1, 1, 0]
+
+
+
+stage = 1
+block = 'a'
+inputs = layers.Input(shape=X.shape[1:])
+
+outputs = conv_block(inputs, kernel_size=2, filters=[2, 4, 6], stage=stage, block=block)
+
+model = Model(inputs=inputs, outputs=outputs)
+prediction = model.predict(X)
+print('identity block output shape:', prediction.shape)
+prediction[1, 1, 0]
